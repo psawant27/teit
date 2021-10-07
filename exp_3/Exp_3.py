74cebcbd5ed63504b97e5366acc284d1734efe84
Rutuja Kurhade <rutujakurhade30@gmail.com>
2021-10-07 10:28:55 +0530

    Exp3

---
+++ exp_3/Exp_3.py
@@ -0,0 +1,52 @@
+print("Program To Check Weather No Is Prime Or Not: ")
+num = int(input("Enter a number: "))  
+  
+if num > 1:  
+   for i in range(2,num):  
+       if (num % i) == 0:  
+           print(num,"is not a prime number")  
+           print(i,"times",num//i,"is",num)  
+           break  
+   else:  
+       print(num,"is a prime number")  
+         
+else:  
+   print(num,"is not a prime number")  
+
+print("To check if the number is an Armstrong number or not")
+
+# Python program to check if the number is an Armstrong number or not
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
+
+# Python program to find the factorial of a number provided by the user.
+# To take input from the user
+print("Program To Find Factorial Of Number! ")
+num = int(input("Enter a number: "))
+factorial = 1
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
