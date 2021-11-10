3c0d9e2c35835b5ea8c49cc8b33d259297347c53
Sakshiii31 <3110swara@gmail.com>
2021-11-10 21:59:19 +0530

    All Files are added Sakshi Awate TY_IT_68

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package p1;
+class Demo 
+{
+	public static void main(String[] args) 
+	{
+		new Protection();
+		new Derived();
+		new SamePackage();
+	}
+}
---
+++ Demo2.java
@@ -0,0 +1,9 @@
+package p2;
+class Demo2 
+{
+	public static void main(String[] args) 
+	{
+		new Protection2();
+		new OtherPackage();
+	}
+}
